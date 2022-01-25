295e33dd59ed55dbf33e6e62d9e00e819d7c87a5
Caio Roberto <caio.roberto@sensedia.com>
2022-01-24 21:19:18 -0300

    feat: created first entity

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,41 @@
+﻿using CatalogApi.Entities;
+using CatalogApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogApi.Controllers
+{   
+    [ApiController]
+    [Route("catalog-api/v1/items")]
+    public class ItemsController : ControllerBase
+    {
+
+        private readonly ItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new ItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return repository.GetItems();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item =  repository.GetItem(id);
+
+            if (item is null)
+                return NotFound();
+
+            return item;
+        }
+    }
+}
---
+++ Repositories/ItemsRepository.cs
@@ -0,0 +1,27 @@
+﻿using CatalogApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogApi.Repositories
+{
+    public class ItemsRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Playstation 5", Price = 7021, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Laptop", Price = 2098, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.SingleOrDefault(item => item.Id == id);
+        }
+    }
+}
